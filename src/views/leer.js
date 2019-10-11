7f381a3d1a1ea1fd0a70f2d39eda051f58dd0cdd
lexisbr <josebarriosrodas@gmail.com>
2019-10-11 00:56:08 -0600

    5to

--- src/views/leer.js
+++ src/views/leer.js
@@ -1,27 +1,61 @@
 
 var input = myForm.myInput;
 var reader = new FileReader;
+var palabrasReservadas = ["if","for","while","int","char","String"]; 
 /*var arreglo = [
   ['posicion1', 'posicion2']
 ];*/
 
-/*var automata = new Array(2);
-automata[0]= new Array(2);
-automata[1]= new Array(2);
-for(var i=0;i<4;i++)
+var automata = new Array(9);
+for(var i=0;i<10;i++)
 {
-  automata[0][i]="S"+i;
+  automata[i]= new Array(10);
+  automata[0][i]= i;
+  console.log(automata[0][i])
 }
-automata[1][0]="letra";
-automata[1][1]="digito";
-for(var i=0;i<automata.length;i++)
+/*for(var i=0;i<automata.length;i++)
 {
-  for(var o=0;o<automata.length;o++){
-    console.log(automata[o][i]);
+  for(var i=0;i<automata[].length;i++)
+  {
+    console.log(automata[0][i])
   }
 }*/
 
-var palabrasReservadas = ["if","for","while","int","char","String"]; 
+
+//[A-Z][a-z]
+automata[1][0]=1;
+automata[1][1]=1;
+//[0-9]
+automata[2][0]=2;
+automata[2][1]=1;
+automata[2][2]=2;
+automata[2][3]=4;
+automata[2][4]=4;
+//[.]
+automata[3][2]=3;
+//[",;]
+automata[4][0]=5;
+//[{,},(,)]
+automata[5][0]=6;
+//[+,-,*,/,=,==,%,>,<]
+automata[6][0]=7;
+//[<,>]
+automata[7][0]=8;
+//[=]
+automata[8][8]=9;
+
+
+
+
+for(var i=0;i<9;i++)
+{
+  for(var o=0;o<10;o++){
+    
+    //console.log(o+" "+automata[i][o]);
+  }
+}
+
+
 input.addEventListener('change', onChange);
 
 
@@ -39,17 +73,43 @@ function onLoad() {
   // En tu ejemplo lo obtienes de una petición HTTP
   
   var lineas = result.split('\n');
-  
+  var caracter;
   
   // o lineas.forEach(function(linea){ ... })
   // o lineas.find(function(linea){ return linea == contraseña })
+  console.log(linea);
   for(var linea of lineas) {
-    for(var i=0; i<3;i++){
-    if(linea === palabrasReservadas[i]) {
+  for(var i=0; i<3;i++){
+    console.log(">>"+linea);
+    for(var o=0;0<10;o++){
+      console.log(">>"+linea.charAt(o));
+    }
+//console.log(">>"+linea.charAt(i));
+    /*  if(linea.value.charAt(0)>=65&&linea.value.charAt(0)<=122){
+        caracter=1;
+      switch(automata[1][i]){
+          case 1:
+              if(linea.value.charAt(i)>=65&&linea.value.charAt(i)<=122&&linea.value.charAt(i)>=48&&linea.value.charAt(i)<=57){
+                
+                switch(automata[1][1]){
+                  case 1:
+
+            
+
+                }
+              }
+            break;
+          default:{
+            co
+          }
+
+      }*/
+    }
+      if(linea === palabrasReservadas[i]) {
      // console.log(input.valueAt(i))
     console.log(linea+" >>Palabra Reservada<<");
     }
-  }
+  
   }
   
 }
